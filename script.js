14463beb99f9d0d7dc5dd3b4b18847010c85d019
Nasif Z Nadif <62985891+notbytes-lab@users.noreply.github.com>
2025-10-08 10:14:31 +0700

    Update script.js
    Separate with ,

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function formatData() {
     const rows = inputData.split('\n');
     
     // Proses setiap baris dan pisahkan elemen berdasarkan koma, titik, atau spasi
-    const formattedData = rows.map(row => row.trim().split(/\s*[.]+\s*/)); // Regex memisahkan berdasarkan koma, titik, atau spasi
+    const formattedData = rows.map(row => row.trim().split(/\s*[,]+\s*/)); // Regex memisahkan berdasarkan koma, titik, atau spasi
 
     // Masukkan data yang sudah diformat ke dalam spreadsheet
     hot.loadData(formattedData);
